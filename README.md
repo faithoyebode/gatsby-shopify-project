cb12146438722ceb87bde6b6614f65e8194278d7
Faith Oyebode <47439363+faithoyebode@users.noreply.github.com>
2022-04-24 13:52:09 +0100

    integrated shopify and added functionality for creating each product page

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gatsby Shopify starter
+
+**Getting started**
+
+First you'll need to add a `.env` file to the root of your project consisting of:
+
+```
+GATSBY_STORE_URL=
+GATSBY_SHOP_PASSWORD=
+```
+
+And enter the appropriate values from your own shopify store.
+
+Then run `yarn start` and you're off to the races!
---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+require('dotenv').config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Shopify`,
+    description: `Gatsby & Shopify`,
+    author: `@faithoyebode`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-optional-chaining`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `open sans\:400`,
+          'open sans:400i',
+          `open sans\:700`,
+          'open sans:700i',
+          `open sans\:800`,
+          'open sans:800i',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        password: process.env.GATSBY_SHOP_PASSWORD,
+        storeUrl: process.env.GATSBY_STORE_URL,
+        shopifyConnections: ['orders', 'collections']
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-shopify-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+  ],
+};
